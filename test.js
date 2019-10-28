8a318a46a66a0912dc5827d9d058b15b78bd5f49
Max Degterev <max@degterev.me>
2019-10-28 13:50:07 +0100

    fix linter

--- test.js
+++ test.js
@@ -4,11 +4,12 @@ const { spawn } = require('child_process');
 const pathOffset = __dirname.split(sep).slice(-2).join(sep);
 const fixturesPath = join(pathOffset, 'test/fixtures');
 
-const env = Object.assign({}, process.env, {
+const env = {
+  ...process.env,
   NODE_ENV: 'customenv',
   CONFIG_DIR: fixturesPath,
   CONFIG_OVERRIDE_KEY: 'sandboxStuffs',
   SANDBOX: 'true',
-});
+};
 
 spawn('mocha', [], { env, stdio: 'inherit' });
